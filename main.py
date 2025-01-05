51f529db8218be21ad52b266a2fb0330ec9de252
Aryan-Kanada <kanadaaryan9@gmail.com>
2025-01-05 21:31:53 +0530

    Use QMainWindow and create file with action of Add student and help with action of about

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QComboBox, QPushButton, \
+    QMainWindow
+from PyQt6.QtGui import QAction
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&help")
+
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+
+
+
+app = QApplication(sys.argv)
+app_class = MainWindow()
+app_class.show()
+sys.exit(app.exec())
