dc0f7efadd2a33d2c97902ff9787eb3f56de61c3
Aryan-Kanada <kanadaaryan9@gmail.com>
2025-01-05 14:45:26 +0530

    Created Widget and add to grid

---
+++ example.py
@@ -0,0 +1,28 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication, QWidget, QLineEdit, QLabel, QPushButton, QGridLayout
+
+
+class AgeCalculator(QWidget):
+    def __init__(self):
+        grid = QGridLayout
+
+        # Create Widget
+        name_lable = QLabel("Name:")
+        name_line_edit = QLineEdit()
+
+        date_lable = QLabel("Date of Birth DD/MM/YY:")
+        date_line_edit = QLineEdit()
+
+        calculate_button = QPushButton("Calculate")
+        output_lable = QLabel("")
+
+        # Add widgets to grid
+        grid.addWidget(name_lable, 0, 0)
+        grid.addWidget(name_line_edit, 0, 1)
+        grid.addWidget(date_lable, 1, 0)
+        grid.addWidget(date_line_edit, 1, 1)
+        grid.addWidget(calculate_button, 2, 0, 1, 2)
+        grid.addWidget(output_lable, 3, 0, 1, 2)
+
+
